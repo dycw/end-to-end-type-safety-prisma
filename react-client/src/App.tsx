a911e3eb65314d8ed6638aeac52a46064a37080f
Derek Wan <d.wan@icloud.com>
2023-06-11 17:42:00 +0800

    Finish lesson 4 (codegen & deployment) (#6)

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,17 +1,16 @@
 import UserDisplay from "./components/UserDisplay";
-import type { User } from "./types";
+
+import { useQuery } from "urql";
+import { GetUsersDocument } from "./graphql/generated";
 
 export default function App() {
-  const users: User[] = [
-    {
-      name: "Sabin Adams",
-      messages: [{ body: "Hey there!" }, { body: "What's up!" }],
-    },
-  ];
+  const [results] = useQuery({
+    query: GetUsersDocument,
+  });
   return (
     <>
       <div className="flex h-screen w-full flex-col items-center justify-center gap-y-12 overflow-scroll bg-zinc-800 p-4">
-        {users.map((user, index) => (
+        {results.data?.users.map((user, index) => (
           <UserDisplay user={user} key={index} />
         ))}
       </div>
