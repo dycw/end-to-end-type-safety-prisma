006fdc25211d4c91e9355bd16ad9323b49fd8703
Derek Wan <d.wan@icloud.com>
2023-06-11 11:58:03 +0800

    Finish lesson 3 (GraphQL API) (#5)

---
+++ graphql-server/src/builder.ts
@@ -0,0 +1,20 @@
+import SchemaBuilder from "@pothos/core";
+import PrismaPlugin from "@pothos/plugin-prisma";
+import type PrismaTypes from "@pothos/plugin-prisma/generated";
+import { DateResolver } from "graphql-scalars";
+import prisma from "./db";
+
+export const builder = new SchemaBuilder<{
+  PrismaTypes: PrismaTypes;
+  Scalars: {
+    Date: { Input: Date; Output: Date };
+  };
+}>({
+  plugins: [PrismaPlugin],
+  prisma: {
+    client: prisma,
+  },
+});
+
+builder.queryType({});
+builder.addScalarType("Date", DateResolver, {});
--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import { createYoga } from "graphql-yoga";
+import { createServer } from "node:http";
+import schema from "./schema";
+
+const port = Number(process.env["API_PORT"]);
+const yoga = createYoga({ schema });
+
+// @ts-expect-error
+const server = createServer(yoga);
+
+server.listen(port, () => {
+  console.log(`GraphQL running at http://localhost:${port}/graphql`);
+});
